a0db6ac0a7e2581018d882a36e07ab48c647397a
Marvelous Mashamba <marvellousmashamba.mm@gmail.com>
2020-11-29 22:47:52 +0200

    yhyhyh

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,5 @@
 import { ExtraOptions, RouterModule, Routes } from '@angular/router';
 import { NgModule } from '@angular/core';
-import {
-  NbAuthComponent,
-  NbLoginComponent,
-  NbLogoutComponent,
-  NbRegisterComponent,
-  NbRequestPasswordComponent,
-  NbResetPasswordComponent,
-} from '@nebular/auth';
 
 export const routes: Routes = [
   {
@@ -17,7 +9,7 @@ export const routes: Routes = [
   },
   {
     path: 'auth',
-    loadChildren: () => import('./pages/auth/auth.module')
+    loadChildren: () => import('./auth/auth.module')
       .then(m => m.AuthModule),
   },
   { path: '', redirectTo: 'auth', pathMatch: 'full' },
